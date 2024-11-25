1d2ae8c99f9f9b11563e58dc03b9ddb354d7724e
ohwooje <dkwooje@naver.com>
2024-11-25 13:17:23 +0900

    react 기초 6

--- 02.DUKUABI/src/components/Examples.jsx
+++ 02.DUKUABI/src/components/Examples.jsx
@@ -7,18 +7,48 @@ function TabButton({ children, onClick }) {
 }
 
 export default function Examples() {
-  function handleClick() {
-    console.log('탭버튼이 클릭 되었습니다.');
+  function handleClick(selectedButton) {
+    console.log(`${selectedButton}버튼이 클릭 되었습니다.`);
   }
 
   return (
     <section id="examples">
       <h2>예시들</h2>
       <menu>
-        <TabButton onClick={handleClick}>컴포넌트</TabButton>
-        <TabButton onClick={handleClick}>JSX</TabButton>
-        <TabButton onClick={handleClick}>Props</TabButton>
-        <TabButton onClick={handleClick}>State</TabButton>
+        {/*
+        <TabButton onClick={function(){handleClick('컴포넌트')}}>컴포넌트</TabButton>
+        <TabButton onClick={function(){handleClick('제이액스')}}>JSX</TabButton>
+        <TabButton onClick={function(){handleClick('프롬프')}}>Props</TabButton>
+        <TabButton onClick={function(){handleClick('스테이트')}}>State</TabButton>
+        */}
+        <TabButton
+          onClick={() => {
+            handleClick('컴포넌트');
+          }}
+        >
+          컴포넌트
+        </TabButton>
+        <TabButton
+          onClick={() => {
+            handleClick('제이액스');
+          }}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          onClick={() => {
+            handleClick('프롬프');
+          }}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          onClick={() => {
+            handleClick('스테이트');
+          }}
+        >
+          State
+        </TabButton>
       </menu>
     </section>
   );
